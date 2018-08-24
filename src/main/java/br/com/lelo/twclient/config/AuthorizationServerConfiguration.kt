040e789b9cd7a55dc54b0ab1d9dc42c274e4d91c
Leo Costa <you@example.com>
2018-08-24 01:54:28 -0300

    Spring cloud

--- src/main/java/br/com/lelo/twclient/config/AuthorizationServerConfiguration.kt
+++ src/main/java/br/com/lelo/twclient/config/AuthorizationServerConfiguration.kt
@@ -4,6 +4,7 @@ import br.com.lelo.twclient.domain.Authorities
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.AuthenticationManager
@@ -12,6 +13,9 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
 import javax.sql.DataSource
 
 
@@ -49,6 +53,21 @@ open class AuthorizationServerConfiguration : AuthorizationServerConfigurerAdapt
                 .scopes("read,write")
                 .secret(secret)
                 .accessTokenValiditySeconds(3600)
+    }
+
+    @Bean
+    open fun corsFilter(): FilterRegistrationBean {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.addAllowedOrigin("*")
+        config.addAllowedHeader("*")
+        config.addAllowedMethod("*")
+        source.registerCorsConfiguration("/**", config)
+
+        val bean = FilterRegistrationBean(CorsFilter(source))
+        bean.order = 0
+        return bean
     }
 
 
